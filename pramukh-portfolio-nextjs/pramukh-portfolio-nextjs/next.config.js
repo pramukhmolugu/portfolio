41b23ec9a6d295505f39fe6831bac1a93580ca34
pramukhmolugu <78114772+pramukhmolugu@users.noreply.github.com>
2026-01-22 00:19:40 -0500

    Update next.config.js

--- pramukh-portfolio-nextjs/pramukh-portfolio-nextjs/next.config.js
+++ pramukh-portfolio-nextjs/pramukh-portfolio-nextjs/next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  output: "export",
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
+  images: { unoptimized: true },
+};
