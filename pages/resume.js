c3d2e9360e8bc77099823e5f1ac57f4802fd986e
pramukhmolugu <78114772+pramukhmolugu@users.noreply.github.com>
2026-01-22 13:26:45 -0500

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -73,7 +73,7 @@ export default function Resume() {
 </a>
           {/* PDF Viewer */}
           <div className="card">
-            <div className="w-full" style={{ height: "85vh" }}>
+            <div className="w-full" style={{ height: "calc(100vh - 200px)" }}>
               <iframe
                 src={pdfPath}
                 title="Resume PDF"
